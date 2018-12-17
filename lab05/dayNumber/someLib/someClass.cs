8b9109d76c3928a60a14cb5614eb78103f7143ff
Pav7u5h34ka <Pav7u5h34ka@yandex.ru>
2018-12-17 16:19:39 +0300

    add lab05

---
+++ lab05/dayNumber/someLib/someClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace someLib
+{
+    public class someClass
+    {
+
+        public string getDatByWeek(int dayNumber)
+        {
+            Hashtable week = new Hashtable();
+            week[1] = "Понедельник";
+            week[2] = "Вторник";
+            week[3] = "Среда";
+            week[4] = "Четверг";
+            week[5] = "Пятница";
+            week[6] = "Суббота";
+            week[7] = "Воскресенье";
+            return week[dayNumber] as string;
+        }
+
+        public int sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int div(int x, int y)
+        {
+            try 
+            {
+                return x / y;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public double div (double x, double y)
+        {
+            if (y == 0) throw new DivideByZeroException();
+            return x / y;
+        }
+    }
+
+}
