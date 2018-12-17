c1d734908807d42ebe640cc906f9d25e6dff0eb7
Pav7u5h34ka <Pav7u5h34ka@yandex.ru>
2018-12-17 15:58:37 +0300

    delete ProgrammingTechnology

---
+++ ProgrammingTechnology/LR5/dayNumber/someProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using someLib;
+
+namespace someProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            someClass some = new someClass();
+            Console.WriteLine(some.sum(2, 2));
+            Console.WriteLine(some.div(4, 2));
+            Console.WriteLine(some.div(4, 0));
+            Console.WriteLine(some.div(4.0, 0));
+            Console.ReadLine();
+        }
+    }
+}
